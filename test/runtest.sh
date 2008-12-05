57e67a5a7e8e3c5b5e2e904b57ad26ef7fb0243d
troy <troy@e3f9d4c8-7941-46de-84ab-374f9122be67>
2008-12-05 23:30:18 +0000

    Better formatting of errors

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-# Create Ouzo script
-./indexes --type uint32 -maxkeys 1 > test.ouzo
-# Feed it to Ouzo
-../ouzo < test.ouzo
+# INDEX_TYPE="int64"
+MAXKEYS="100"
 
-# Get results from Ouzo and format it for comparison
-../ouzo "show uint32_test" | sed -e '1,/^\s*$/d' -e '/^\s*$/d' | perl -n -e 'chomp;($k,$d)=split(/:/);@d=split(/\s+/,$d);foreach $n (@d) { print "$k:$n\n";}'|sort > actual.out
-# Format test script's output
-grep "^(" test.ouzo | sed -e 's/(//' -e 's/)//' -e 's/;//' | perl -n -e 'chomp;($k,$d)=split(/:/);@d=split(/\s+/,$d);foreach $n (@d) { print "$k:$n\n";}'|sort -u > expected.out
+OUZO_PROG="../ouzo"
 
-# Compare the two
-diff -u expected.out actual.out
+function runtest {
+	INDEX_TYPE=$1
+
+	echo "Running $INDEX_TYPE test...";
+	
+	# Create Ouzo script
+	./indexes --type $INDEX_TYPE -maxkeys $MAXKEYS > test.ouzo
+	# Feed it to Ouzo
+	$OUZO_PROG < test.ouzo
+	
+	# Get results from Ouzo and format it for comparison
+	$OUZO_PROG "show $INDEX_TYPE""_test" | sed -e '1,/^\s*$/d' -e '/^\s*$/d' | perl -n -e 'chomp;($k,$d)=split(/:/);@d=split(/\s+/,$d);foreach $n (@d) { print "$k:$n\n";}'|sort > actual.out
+	# Format test script's output
+	grep "^(" test.ouzo | sed -e 's/(//' -e 's/)//' -e 's/;//' | perl -n -e 'chomp;($k,$d)=split(/:/);@d=split(/\s+/,$d);foreach $n (@d) { print "$k:$n\n";}'|sort -u > expected.out
+	
+	# Compare the two
+	if ! diff -u actual.out expected.out > runtest.diff; then
+		MISSING=`egrep -c "^-" runtest.diff`
+		EXTRA=`egrep -c "^\+" runtest.diff`
+		echo "$INDEX_TYPE test failed: $MISSING missing, $EXTRA extra keys";
+	else
+		rm -f runtest.diff actual.out expected.out *.index test.ouzo
+	fi
+}
+
+
+TYPES="int8 int16 int32 int64 uint8 uint16 uint32 uint64 double char8 date time";
+
+for T in $TYPES; do
+	runtest $T
+done
